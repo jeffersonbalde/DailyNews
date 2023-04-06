094bea70df59a4dc75fa51074a88128b9860e0fe
Jefferson Balde <jeffersonbalde8@gmail.com>
2023-04-06 14:55:44 +0800

    fixing darkmode bugs

--- app/DarkModeButton.tsx
+++ app/DarkModeButton.tsx
@@ -20,9 +20,15 @@ function DarkModeButton() {
     return (
         <div>
             {currentTheme === "dark" ? (
-
+                <SunIcon 
+                    className="h-8 w-8 cursor-pointer text-yellow-500"
+                    onClick={() => setTheme("light")}
+                />
             ) : (
-                
+                <MoonIcon 
+                    className="h-8 w-8 cursor-pointer text-gray-900"
+                    onClick={() => setTheme("dark")}
+                />
             )}
         </div>
     )
